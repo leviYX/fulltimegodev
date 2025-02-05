022dd910f52f953b31b3be1985bc51f3788c4def
liuwenqiang <wenqiang.liu@wenge.com>
2025-02-05 12:32:09 +0800

    语言特性操作

--- main.go
+++ main.go
@@ -1,21 +1,103 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
+
+var name = "John Doe"
+var age int
+
+const (
+	version = 1
+	key     = 10
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+type Person struct {
+	name string
+	age  int8
+
+	score struct {
+		math    int8
+		english int8
+	}
+}
 
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
+	// structDemo()
+	// mapDemo1()
+	//mapDemo3()
+	// fmt.Printf("damage:%d\n", getDamage("ord"))
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+}
+
+func getDamage(weaponType string) int {
+	switch weaponType {
+	case "sword":
+		return 10
+	case "gun":
+		return 20
+	default:
+		panic("unknow weapon type")
 	}
+}
+
+func mapDemo1() {
+	var cars map[string]string = map[string]string{
+		"benz": "mercedes",
+		"bmw":  "bmw",
+		"audi": "audi",
+	}
+	fmt.Println(cars)
+	// 删除map中的某个key
+	delete(cars, "benz")
+}
+
+func mapDemo2() {
+	var users map[string]int = map[string]int{
+		"levi":  18,
+		"hanji": 20,
+		"ailun": 14,
+	}
+
+	leviAge, hasFlag := users["levi"]
+	if hasFlag {
+		fmt.Printf("levi age is %d \n", leviAge)
+	} else {
+		fmt.Println("levi age is not exist")
+	}
+}
+
+func mapDemo3() {
+	var users map[string]int = make(map[string]int)
+	users["levi"] = 18
+	users["hanji"] = 20
+	users["ailun"] = 14
+	for k, v := range users {
+		fmt.Printf("key:%s,value:%d\n", k, v)
+	}
+
+	fmt.Println("----------")
+	for k := range users {
+		fmt.Printf("key:%s \n", k)
+	}
+
+	fmt.Println("----------")
+
+	for _, v := range users {
+		fmt.Printf("value:%d \n", v)
+	}
+}
+
+func structDemo() {
+	var p1 = Person{
+		name: "John Doe",
+		age:  18,
+		score: struct {
+			math    int8
+			english int8
+		}{
+			math:    90,
+			english: 80,
+		},
+	}
+
+	fmt.Printf("%+v\n", p1)
 }
