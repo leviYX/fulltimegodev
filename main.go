85a6a9595d815bef140cfb3f1d47d1d3718b1495
liuwenqiang <wenqiang.liu@wenge.com>
2025-02-06 19:12:51 +0800

    模块化，以及go得makefile

--- main.go
+++ main.go
@@ -5,7 +5,53 @@ import (
 	"myprojectmod/utils"
 )
 
-func main() {
+type Entity struct {
+	version    string
+	name       string
+	id         int
+	posx, posy int
+}
+
+type specialEntity struct {
+	entity       Entity
+	specialField string
+}
+
+type demoEntity struct {
+	Entity
+	specialField string
+}
+
+func main1() {
+	e1 := Entity{
+		version: "1.0.0",
+		name:    "John",
+		id:      1,
+		posx:    100,
+		posy:    200,
+	}
+
+	e2 := demoEntity{
+		Entity:       e1,
+		specialField: "special",
+	}
+
+	e3 := specialEntity{
+		entity: Entity{
+			version: "1.0.0",
+			name:    "John",
+			id:      1,
+			posx:    100,
+			posy:    200,
+		},
+		specialField: "special",
+	}
+
+	fmt.Printf("e1 data is %+v\ne2 data is %+v\ne3 data is %+v\n", e1, e2, e3)
+	fmt.Println(e3.entity.name, e2.name)
+}
+
+func main0() {
 
 	user := utils.User{
 		UserName: "John",
---
+++ main2.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type Color int
+
+const (
+	Red Color = iota
+	Blue
+)
+
+type DataInterface interface {
+	String() string
+}
+
+/*
+*
+实现了String接口，所以传入Color类型就会调用该方法。
+go的接口实现，只要你满足了接口的所有方法，就实现了该接口。并且你的func(类型)这就表示这个类型实现了接口。
+*/
+func (c Color) String() string {
+	switch c {
+	case Red:
+		return "red"
+	case Blue:
+		return "blue"
+	default:
+		return "unknown color"
+	}
+}
+
+func main() {
+	/**
+	Println 方法会调用参数的String 方法，然后我的func (c Color) String() string 实现了该方法，所以会打印出对应的颜色。
+	等于是String的接口实现，这里Color实现了String接口。所以传入Color类型，就会调用该方法。
+	*/
+	fmt.Println(Red)
+}
